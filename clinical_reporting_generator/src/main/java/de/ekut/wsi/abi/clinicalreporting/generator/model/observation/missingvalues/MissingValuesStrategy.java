8a51e9b40be5988caeeec1dfe4869af83239a557
Bilge Surun <bilges@whatever.Informatik.Uni-Tuebingen.De>
2018-03-05 18:18:20 +0100

    clinical_reporting_generator folder is removed.

--- clinical_reporting_generator/src/main/java/de/ekut/wsi/abi/clinicalreporting/generator/model/observation/missingvalues/MissingValuesStrategy.java
+++
@@ -1,8 +0,0 @@
-package de.ekut.wsi.abi.clinicalreporting.generator.model.observation.missingvalues;
-
-import de.ekut.wsi.abi.clinicalreporting.generator.model.observation.ObservationBuilder;
-
-public interface MissingValuesStrategy {
-
-	String resolve(final ObservationBuilder b);
-}
