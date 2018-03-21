80a3463a0c3891ffa36a08b33d9cae7c8f23d3e7
Bilge Surun <bilges@whatever.Informatik.Uni-Tuebingen.De>
2018-03-21 14:44:05 +0100

    minor changes

--- ReportApp/make_report.sh
+++ ReportApp/make_report.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+while [ ! -f /data/completeness.flag ];
+do 
+sleep 30
+done
+
 OPTIND=1
 while getopts "t:p:" opt; do
     case $opt in
@@ -23,26 +28,26 @@ echo "################ $outfile is created. ################"
 echo "################ Start to create json ################"
 Rscript /home/vep/reporting.R -f $infile.out.vcf  && \
 echo "################ JSON is created  ################"
-cp base.log /data
+cp base.log /inout
 
 if [[ $savedOut == *"j"* ]]; then
-    cp report.json /data
+    cp report.json /inout
     echo "JSON is saved to the volume"
 fi
 
 echo "################ Start to create report ################"
-nodejs /data/clinicalreporting_docxtemplater/main.js -d report.json -t /data/clinicalreporting_docxtemplater/data/template.docx -o out.docx && \
+nodejs /home/vep/clinicalreporting_docxtemplater/main.js -d report.json -t /home/vep/clinicalreporting_docxtemplater/data/template.docx -o out.docx && \
 echo "################ Report is created  ################"
 if [[ $savedOut == *"w"* ]]; then
-    cp out.docx /data
+    cp out.docx /inout
     echo "Report is saved to the volume as DOCX file."
 fi
 
     # convert it to pdf
 if [[ $savedOut == *"p"* ]]; then
     echo "################ Start to create pdf ################"
-    libreoffice --headless --convert-to pdf /data/out.docx && \
+    libreoffice --headless --convert-to pdf /inout/out.docx && \
     echo "################ pdf is created  ################"
-    cp out.pdf /data && \
+    cp out.pdf /inout && \
     echo "Report is saved to the volume as PDF file."
 fi
